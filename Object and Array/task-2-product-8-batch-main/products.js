00d178bb54005202093672a973d7be2491466d16
Hrishikesh <sarma10700@gmail.com>
2024-09-02 17:37:01 +0530

    task 02-09-24

---
+++ Object and Array/task-2-product-8-batch-main/products.js
@@ -0,0 +1,116 @@
+const products = [
+  {
+    id: 'rec43w3ipXvP28vog',
+    title: 'high-back bench',
+    company: 'ikea',
+    image:
+      'https://www.leathersofaworld.com/pub/media/catalog/product/cache/3530038e3fc4aa6e501b6b84a7eba76d/l/u/luxe-grey-bench-roomset_xv9mmbv4yg9a9fro.jpg',
+    price: 9.99,
+  },
+  {
+    id: 'rec4f2RIftFCb7aHh',
+    title: 'albany table',
+    company: 'marcos',
+    image:
+      'https://sothebys-md.brightspotcdn.com/dims4/default/5973012/2147483647/strip/true/crop/2000x2000+0+0/resize/4096x4096!/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fmedia-desk%2Fwebnative%2Fimages%2Fd1%2Ff1%2Faecdec2b431cbb4ff2603ef8f67f%2Fl23350-ctw7d-t2-03.jpg',
+    price: 79.99,
+  },
+  {
+    id: 'rec4f2RIftFCb7aHh',
+    title: 'albany table',
+    company: 'marcos',
+    image:
+      'https://sothebys-md.brightspotcdn.com/dims4/default/5973012/2147483647/strip/true/crop/2000x2000+0+0/resize/4096x4096!/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fmedia-desk%2Fwebnative%2Fimages%2Fd1%2Ff1%2Faecdec2b431cbb4ff2603ef8f67f%2Fl23350-ctw7d-t2-03.jpg',
+    price: 788.99,
+  },
+  {
+    id: 'rec4f2RIftFCb7aHh',
+    title: 'albany table',
+    company: 'marcos',
+    image:
+      'https://sothebys-md.brightspotcdn.com/dims4/default/5973012/2147483647/strip/true/crop/2000x2000+0+0/resize/4096x4096!/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fmedia-desk%2Fwebnative%2Fimages%2Fd1%2Ff1%2Faecdec2b431cbb4ff2603ef8f67f%2Fl23350-ctw7d-t2-03.jpg',
+    price: 1279.99,
+  },
+  {
+    id: 'rec8kkCmSiMkbkiko',
+    title: 'accent chair',
+    company: 'caressa',
+    image:
+      'https://m.media-amazon.com/images/I/81dcxsgGgYL._AC_UF894,1000_QL80_.jpg',
+    price: 25.99,
+  },
+  {
+    id: 'recBohCqQsot4Q4II',
+    title: 'wooden table',
+    company: 'caressa',
+    image:
+      'https://woodsala.com/cdn/shop/files/MG_2527.jpg?v=1709635845',
+    price: 45.99,
+  },
+  {
+    id: 'recDG1JRZnbpRHpoy',
+    title: 'dining table',
+    company: 'caressa',
+    image:
+      'https://i.pinimg.com/736x/14/4e/6c/144e6c4e1f8cce0c24600b2999d90318.jpg',
+    price: 6.99,
+  },
+  {
+    id: 'recNWGyP7kjFhSqw3',
+    title: 'sofa set',
+    company: 'liddy',
+    image:
+      'https://images.woodenstreet.de/image/data/fabric-sofa/Mellisa-fabric-sofa/new-logo/teak/1.jpg',
+    price: 69.99,
+  },
+  {
+    id: 'recZEougL5bbY4AEx',
+    title: 'modern bookshelf',
+    company: 'marcos',
+    image:
+      'https://m.media-amazon.com/images/I/81+M-GvWk8L._AC_UF894,1000_QL80_.jpg',
+    price: 8.99,
+  },
+  {
+    id: 'recjMK1jgTb2ld7sv',
+    title: 'emperor bed',
+    company: 'liddy',
+    image:
+      'https://static.standard.co.uk/2022/04/07/12/Rust%20Verbier%20bed%20landscape.jpg?crop=8:5,smart&quality=75&auto=webp&width=1000',
+    price: 21.99,
+  },
+  {
+    id: 'recmg2a1ctaEJNZhu',
+    title: 'utopia sofa',
+    company: 'marcos',
+    image:
+      'https://i.pinimg.com/736x/03/1a/0b/031a0b2802905e22637feab32d69b7f5.jpg',
+    price: 39.95,
+  },
+  {
+    id: 'recvKMNR3YFw0bEt3',
+    title: 'entertainment center',
+    company: 'liddy',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRIbGdPN-oBu8XdZ-yZFXUpS6ue66W4mFLxg&s',
+    price: 29.98,
+  },
+  {
+    id: 'recxaXFy5IW539sgM',
+    title: 'albany sectional',
+    company: 'ikea',
+    image:
+      'https://cdn11.bigcommerce.com/s-mhjjd3e/images/stencil/1920x1280/products/2485/10300/albany-938-tweed-toast-sectional__63100.1705018920.jpg?c=2',
+    price: 10.99,
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    title: 'leather sofa',
+    company: 'liddy',
+    image:
+      'https://images.durian.in/Durian/durian/product/800x800/130381673608206.jpg',
+    price: 9.99,
+  },
+];
+
+
