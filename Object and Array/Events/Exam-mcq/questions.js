276229f27680ecacd03028e6997ebb9c08515a56
Hrishikesh <sarma10700@gmail.com>
2024-09-16 21:12:14 +0530

    MCQ example for events

---
+++ Object and Array/Events/Exam-mcq/questions.js
@@ -0,0 +1,55 @@
+let data = {
+    "examTitle": "JavaScript Multiple Choice Exam",
+    "questions": [
+      {
+        "question": "What is the correct syntax to print a message in the console in JavaScript?",
+        "choices": [
+          "console.log('message');",
+          "print('message');",
+          "echo('message');",
+          "log.console('message');"
+        ],
+        "correctAnswer": "console.log('message');"
+      },
+      {
+        "question": "Which operator is used to compare values for equality in JavaScript?",
+        "choices": [
+          "=",
+          "===",
+          "==",
+          "equals"
+        ],
+        "correctAnswer": "=="
+      },
+      {
+        "question": "What does the 'typeof' operator do in JavaScript?",
+        "choices": [
+          "It returns the data type of a variable.",
+          "It returns the value of a variable.",
+          "It checks if a variable is defined.",
+          "It converts a variable to a string."
+        ],
+        "correctAnswer": "It returns the data type of a variable."
+      },
+      {
+        "question": "How do you create a function in JavaScript?",
+        "choices": [
+          "function myFunction() {}",
+          "create myFunction() {}",
+          "def myFunction() {}",
+          "function:myFunction() {}"
+        ],
+        "correctAnswer": "function myFunction() {}"
+      },
+      {
+        "question": "What will be the result of '5 + '5'' in JavaScript?",
+        "choices": [
+          "10",
+          "'55'",
+          "Error",
+          "'5 + 5'"
+        ],
+        "correctAnswer": "'55'"
+      }
+    ]
+  }
