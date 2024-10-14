ec85417ca3b09e23dfabcdbbe5ae5f345bdf8db3
Hrishikesh <sarma10700@gmail.com>
2024-10-15 00:16:27 +0530

    Callfunc

--- Project/Job portal JS/frontend/common/nav.js
+++ Project/Job portal JS/frontend/common/nav.js
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
-
-    <!-- Bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+let navbar = `<nav class="navbar navbar-expand-lg bg-primary navbar-dark">
         <div class="container ">
             <a class="navbar-brand" href="#">Job Portal</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -23,10 +9,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="welcome-recuiter.html">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active"  href="#">Add Jobs</a>
+                        <a class="nav-link active"  href="addjobs.html">Add Jobs</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active"  href="#">My Jobs</a>
@@ -45,7 +31,4 @@
 
             </div>
         </div>
-    </nav>
-</body>
-
-</html>
+    </nav>`
